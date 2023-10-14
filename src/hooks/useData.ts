c816441ce7c8cc39253683c06bd33317b36b2271
Your Name <you@example.com>
2023-10-14 17:42:33 +0800

    import react query

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -3,7 +3,7 @@ import apiClients from "../services/api-clients";
 import { AxiosRequestConfig, CanceledError } from "axios";
 
 
-interface FetchResponse<T> {
+export interface FetchResponse<T> {
     count: number;
     results: T[]
 }

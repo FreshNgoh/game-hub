7e13491fc168bc0d618fd90cbc555d9eebf4c236
Your Name <you@example.com>
2023-10-15 14:53:38 +0800

    Refactoring fetch game with react-query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
+import { FetchResponse } from "../services/api-clients";
+import apiClients from "../services/api-clients";
 
 export interface Platform {
     id: number;
@@ -16,15 +18,17 @@ export interface Game {
     rating_top: number;
 }
 
-const useGames = (gameQuery: GameQuery) => 
-  useData<Game>('/games', {
-  params: {
-    genres: gameQuery.genre?.id, 
-    platforms: gameQuery.platform?.id,
-    ordering: gameQuery.sortOrder,
-    search: gameQuery.searchText
-    }
-  }, 
-  [gameQuery]);
+const useGames = (gameQuery: GameQuery) => useQuery<FetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () => apiClients.get<FetchResponse<Game>>('/games', {
+      params: {
+        genres: gameQuery.genre?.id,
+        parent_Peplatforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText
+      }
+    }).then(res => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+  })
 
 export default useGames;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClients from "../services/api-clients";
-import { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/api-clients";
 import platforms from "../data/platforms";
 
 interface Platform {

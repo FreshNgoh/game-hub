b7831c8aa821dac733a691c0d57915becda072e9
Your Name <you@example.com>
2023-10-16 16:19:12 +0800

    refactoring duplicate interface

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -3,7 +3,7 @@ import apiClients from "../services/api-clients";
 import { FetchResponse } from "../services/api-clients";
 import platforms from "../data/platforms";
 
-interface Platform {
+export interface Platform {
     id: number;
     name: string;
     slug: string;

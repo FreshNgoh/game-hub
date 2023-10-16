63128b867aea76080310d199d072b807916d0042
Your Name <you@example.com>
2023-10-16 20:41:44 +0800

    Implement infinite query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-clients";
 import { Platform } from "./usePlatforms";
@@ -14,16 +14,19 @@ export interface Game {
     rating_top: number;
 }
 
-const useGames = (gameQuery: GameQuery) => useQuery<FetchResponse<Game>, Error>({
+const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
-    queryFn: () => apiClient.getAll({
+    queryFn: ({ pageParam = 1 }) => apiClient.getAll({
     params: {
       genres: gameQuery.genre?.id,
       parent_Peplatforms: gameQuery.platform?.id,
       ordering: gameQuery.sortOrder,
-      search: gameQuery.searchText
+      search: gameQuery.searchText,
+      page: pageParam
     }}),
-    staleTime: 24 * 60 * 60 * 1000,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    }
   })
 
 export default useGames;

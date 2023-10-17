a8d83921d1d134988ff5c4089584359348a51b83
Your Name <you@example.com>
2023-10-17 15:31:59 +0800

    Refactoring- create look-up hooks

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,21 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
-import APIClient from "../services/api-clients";
-import { FetchResponse } from "../services/api-clients";
-import genres from "../data/genres";
+import useGenres from "./useGenres";
 
-const apiClient = new APIClient<Genre>('/genres')
-
-export interface Genre {
-    id: number;
-    name: string;
-    image_background: string;
+const useGenre = (id?: number) => {
+    const { data: genres } = useGenres();
+    return genres?.results.find((g) => g.id === id);
 }
 
-const useGenres = () => useQuery({
-    queryKey: ['genres'],
-    queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24h
-    initialData: {count: genres.length, results: genres}
-})
-
-export default useGenres;
+export default useGenre;

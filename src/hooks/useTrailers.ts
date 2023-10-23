461a55c0d1fa029c3b4be9fde83d7f785d82e4fc
Your Name <you@example.com>
2023-10-23 16:34:07 +0800

    Refactor error

--- src/hooks/useTrailers.ts
+++ src/hooks/useTrailers.ts
@@ -6,8 +6,8 @@ const useTrailers = (gameId: number) => {
   const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
 
   return useQuery({
-    queryKey: ['trialers', gameId],
-    queryFn: apiClient.getAll
+    queryKey: ['trailers', gameId],
+    queryFn: apiClient.getAll,
 })}
 
 export default useTrailers;
